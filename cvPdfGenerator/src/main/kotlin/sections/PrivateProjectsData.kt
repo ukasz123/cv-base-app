c89468cdcf4f5a2db6765d272396a010091c8317
Łukasz Huculak <ukasz123@gmail.com>
2019-12-16 20:22:14 +0100

    Add skills icons

--- cvPdfGenerator/src/main/kotlin/sections/PrivateProjectsData.kt
+++ cvPdfGenerator/src/main/kotlin/sections/PrivateProjectsData.kt
@@ -1,14 +1,14 @@
 package sections
 
 import CVMeta
-import pl.ukaszapps.itext.nodes.Column
-import pl.ukaszapps.itext.nodes.Node
-import pl.ukaszapps.itext.nodes.Text
+import pl.ukaszapps.itext.nodes.*
 import sections.common.*
 import sections.common.defaultBoldFont
 import sections.common.defaultFont
 import sections.common.getOrError
 import sections.common.parse
+import sections.icons.Skills
+import sections.icons.techIcon
 import java.io.File
 
 data class PrivateProject(
@@ -24,15 +24,25 @@ fun privateProjectsSection(metadata: CVMeta): Node {
   val translations = metadata.translations
   val privateProjectsFile = File(metadata.baseDir, "data/other_projects/${metadata.lang}.json")
   val privateProjectsData = parse<PrivateProjects>(privateProjectsFile)
+  println()
   return Column(
       children = listOf(
           listOf(sectionText(translations.getOrError("otherProjects"))),
-          privateProjectsData.projects.map {
+          privateProjectsData.projects.mapIndexed { index, it ->
+            Padding(child =
             Column(children = listOf(
                 Text(it.title, font = defaultBoldFont),
+                Row(children = it.skills.map {skillName ->
+                  Skills.valueOf(skillName.toUpperCase())
+                }.map { skill -> techIcon(skill) },
+                    weights = (0..it.skills.size-2)
+                        .map { 0.075f } + (1f - 0.075f* (it.skills.size-2))
+                ),
                 Text(it.description, font = defaultFont),
                 Text(it.repositoryUrl, font = captionFont)
-            ))
+            )),
+                padding = Dimensions(top = if (index > 0) 16f else 0f)
+            )
           }
       ).flatten()
   )
